0144f0de2425ab3836819757a794899312ec28a3
liorkazaz <lkazaz@prodware365.com>
2023-04-10 18:24:02 +0300

    test

--- 15.py
+++ 15.py
@@ -1,7 +1,8 @@
 import requests
 def check_if_url_uo(url):
+    dd = 1234
     response = requests.get(url)
-    if 200 <= response.status_code < 300:
+    if 200 <= response.status_code < 350:
         return True
     return False
 
